6a9539f1132ee4e133275f21645488a2f5177b1c
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2021-07-25 19:46:29 +0300

    Add files via upload

---
+++ Polymorphism/Vehicles/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Vehicles
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine()
+                .Split()
+                .ToArray();
+            string[] truckInfo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            Vehicle car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            Vehicle truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+                string command = input[0];
+                string type = input[1];
+                double amount = double.Parse(input[2]);
+
+                if (command is "Drive")
+                {
+                    if (type is "Car")
+                    {
+                        CanDrive(car, amount);
+                    }
+                    else
+                    {
+                        CanDrive(truck, amount);
+                    }
+                }
+                else // refuel
+                {
+                    if (type is "Car")
+                    {
+                        car.Refuel(amount);
+                    }
+                    else
+                    {
+                        truck.Refuel(amount);
+                    }
+
+                }
+                    
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:F2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:F2}");
+        }
+
+        public static void CanDrive(Vehicle vehicle, double distance)
+        {
+            bool canDrive = vehicle.CanDrive(distance);
+            string vehicleType = vehicle.GetType().Name;
+            string result = canDrive ? $"{vehicleType} travelled {distance} km"
+                : $"{vehicleType} needs refueling";
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Polymorphism/Vehicles/Truck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double AirConConsumption = 1.6;
+        public Truck(double fuelConsumption, double fuelQuantity) : base(fuelConsumption, fuelQuantity)
+        {
+
+        }
+
+        public override double FuelConsumption => base.FuelConsumption + AirConConsumption;
+
+        public override void Refuel(double amount)
+        {
+            base.Refuel(amount * 0.95);
+        }
+    }
+}
