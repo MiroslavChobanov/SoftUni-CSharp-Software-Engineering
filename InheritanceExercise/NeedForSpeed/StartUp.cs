08e06c73f99dcc818a524cb6483ab8d75af427f1
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2021-11-23 17:34:22 +0200

    Add files via upload

---
+++ InheritanceExercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle motor = new RaceMotorcycle(100, 200);
+
+            motor.Drive(2);
+        }
+    }
+}
---
+++ InheritanceExercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+            System.Console.WriteLine($"Km driven --> {kilometers}");
+        }
+
+        
+    }
+}
