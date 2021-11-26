d9671abd6fedabdf6deb7b417b2c7141203af5b4
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2021-11-27 00:53:21 +0200

    Add files via upload

---
+++ C#Databases/DB Advanced/05JsonProcessing/CarDealer/CarDealer/DTO/ExportDtos/CarPartsDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO.ExportDtos
+{
+    public class CarPartsDto
+    {
+        [JsonProperty("car")]
+        public ExportCarDto Car { get; set; }
+
+        [JsonProperty("parts")]
+        public ICollection<ExportPartDto> Parts { get; set; }
+    }
+}
