a5f44dbd4fb2087523afca35a29af4ef86f9faf0
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2021-11-27 00:56:32 +0200

    Delete BookShop directory

--- BookShop/BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString 
-            => "Server=(localdb)\\MSSQLLocalDB;Database=BookShop;Integrated Security=True;";
-    }
-}
