87777038916742b329708f0e90da9b7ae4df14b3
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2021-12-12 13:32:06 +0200

    Add files via upload

---
+++ C# Advanced/Exercises/JaggedArrays/JaggedArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> counter = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counter.ContainsKey(number))
+                {
+                    counter[number]++;
+                }
+                else
+                {
+                    counter.Add(number, 1);
+                }
+            }
+
+            foreach (var kvp in counter)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+
+        }
+    }
+}
