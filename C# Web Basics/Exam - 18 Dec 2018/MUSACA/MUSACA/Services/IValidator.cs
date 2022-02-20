29406755de112108a4249b6f6c7d2d6d1162e4a8
MiroslavChobanov <81228336+MiroslavChobanov@users.noreply.github.com>
2022-02-21 01:43:58 +0200

    Add files via upload

---
+++ C# Web Basics/Exam - 18 Dec 2018/MUSACA/MUSACA/Services/IValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace MUSACA.Services
+{
+    using System.Collections.Generic;
+    using MUSACA.ViewModels.Users;
+
+    public interface IValidator
+    {
+        ICollection<string> ValidateUser(RegisterUserFormModel model);
+
+    }
+}
